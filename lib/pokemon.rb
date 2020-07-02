4d14a52559012d78b15f3ca450ea81257b022a8e
Elijah Kirschen <ekirschen584@gmail.com>
2020-07-02 21:46:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+    attr_reader :id, :db
+    attr_accessor :name, :type
+
+    def initialize(id:, name:, type:, db:)
+        @name =name
+        @type = type
+        @db = db
+        @id= id
+    end
+
+    def self.save(name, type, db)
+        sql =  "
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?)
+        "
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql= "
+        SELECT * FROM pokemon
+        WHERE id = ?
+        "
+        info = db.execute(sql, id).flatten
+        Pokemon.new(id: info[0], name: info[1], type: info[2], db: db)
+    end
+
 end
